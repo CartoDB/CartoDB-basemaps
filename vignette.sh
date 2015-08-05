9335983f87101f4078ccec864f9bd08d99c264a5
Seth Fitzsimmons <seth@mojodna.net>
2015-08-04 17:41:37 -0700

    CI configuration

---
+++ vignette.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+for filename in *.yml; do
+  name=$(node_modules/.bin/js-yaml $filename | jq -r .name)
+
+  if [ ! $name == "null" ]; then
+    echo "Rendering ${name}"
+    node_modules/.bin/vignette -o vignettes/${name} -c places.json -w 600 -h 400 cartodb:///${name}
+  fi
+done

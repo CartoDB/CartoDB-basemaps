384b9b1aebb84706c038bb62ca9d349af4a87b5b
Seth Fitzsimmons <seth@mojodna.net>
2015-08-04 17:46:04 -0700

    Unexpected operator fixes

--- vignette.sh
+++ vignette.sh
@@ -3,7 +3,7 @@
 for filename in *.yml; do
   name=$(node_modules/.bin/js-yaml $filename | jq -r .name)
 
-  if [ ! $name == "null" ]; then
+  if [ ! "$name" == "null" ]; then
     echo "Rendering ${name}"
     node_modules/.bin/vignette -o vignettes/${name} -c places.json -w 600 -h 400 cartodb:///${name}
   fi

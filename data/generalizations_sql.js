53a987f5a3146c2d94a48c72acd07ff53cbb37f9
Paul Norman <penorman@mac.com>
2016-03-02 03:41:15 -0800

    Allow for non-materialized views
    Fixes #49

--- data/generalizations_sql.js
+++ data/generalizations_sql.js
@@ -14,16 +14,26 @@ if (process.argv.length == 2) {
   console.log("SET client_min_messages TO WARNING;");
   console.log("SET statement_timeout = 0;\n")
   doc.forEach(function(view) {
+    var pg_type = 'VIEW';
+    if (view.materialized) {
+      var pg_type = 'MATERIALIZED VIEW';
+    }
     console.log("DROP "+pg_type+" IF EXISTS " + tname(view.name) + " CASCADE;");
-    console.log("CREATE "+pg_type+" " + tname(view.name) + " AS" +
-                " SELECT id, " + view.select +
-                " FROM "  + tname(view.from) + 
-                " WHERE " + view.where + 
-                " ORDER BY ST_GeoHash(ST_Transform(ST_SetSRID(Box2D(" + view.cluster_on + "), 3857), 4326));");
-    console.log("CREATE INDEX " + view.name + "_" + view.index_by + "_gist ON " + 
-                 tname(view.name) + " USING gist(" + view.index_by + ");");
-    console.log("CREATE UNIQUE INDEX ON " + tname(view.name) + " (id);");
-    console.log("ANALYZE " + tname(view.name) + ";\n");
+    console.log("CREATE "+pg_type+" " + tname(view.name) + " AS");
+    console.log("  SELECT id, " + view.select);
+    console.log("    FROM "  + tname(view.from));
+    console.log("    WHERE " + view.where);
+    if (pg_type == 'MATERIALIZED VIEW') {
+      console.log("    ORDER BY ST_GeoHash(ST_Transform(ST_SetSRID(Box2D(" + view.cluster_on + "), 3857), 4326));");
+      console.log("CREATE INDEX " + view.name + "_" + view.index_by + "_gist ON " +
+                   tname(view.name) + " USING gist(" + view.index_by + ");");
+      console.log("CREATE UNIQUE INDEX ON " + tname(view.name) + " (id);");
+      console.log("ANALYZE " + tname(view.name) + ";\n");
+    } else {
+      console.log(";"); // Don't ORDER BY
+      console.log("CREATE INDEX IF NOT EXISTS \"" + view.from + "_view_" + view.name + "_idx\" ON " + tname(view.from) +
+                  " USING GIST (" + view.cluster_on + ") WHERE (" + view.where + ");");
+    }
   });
   console.log("RESET client_min_messages;");
 }
